2188790b5a53172bf5cd1292473d6dcadeb874fe
GarageGoose <garagegoosegames@gmail.com>
2026-06-04 23:36:35 +0800

    (WIP) Implemented graph operations on GraphOp2D

--- NodeGraph/Implementation/GraphOperations/GraphOperations3D.cs
+++ NodeGraph/Implementation/GraphOperations/GraphOperations3D.cs
@@ -2,17 +2,42 @@ namespace GG.NodeGraph.Implementations;
 
 public static class GraphOp3D
 {
-    public static void CollapseNode(IGraph<Node3D> baseGraph, uint[] nodeIDsToCollapse)
+    public static void CopyElementsToGraph(IGraph<Node3D> copyFrom, IEnumerable<ElementID> elementsToCopy, IGraph<Node3D> pasteTo, bool preserveID = false)
     {
         
     }
 
-    public static Node3D SplitEdge(IGraph<Node3D> baseGraph, uint edgeID, float splitPos = 0.5f)
+    public static void CopyGraph(IGraph<Node3D> copyFrom, IGraph<Node3D> pasteTo)
     {
         
     }
 
-    public static IEnumerable<Node3D> SplitEdgeMultiple(IGraph<Node3D> baseGraph, uint edgeID, uint SplitAmount)
+    public static void InsertNode(IGraph<Node3D> baseGraph, uint edgeID, Node3D newNode)
+    {
+        
+    }
+
+    public static void CollapseNode(IGraph<Node3D> baseGraph, params uint[] nodeIDsToCollapse)
+    {
+        
+    }
+
+    public static void BypassRemoveNode(IGraph<Node3D> baseGraph, uint nodeID)
+    {
+        
+    }
+
+    public static void SplitNode(IGraph<Node3D> baseGraph, uint edgeID, Node3D[] newNodes)
+    {
+        
+    }
+
+    public static Node2D SplitEdge(IGraph<Node3D> baseGraph, uint edgeID, float splitPos = 0.5f)
+    {
+        
+    }
+
+    public static IEnumerable<Node2D> SplitEdgeMultiple(IGraph<Node3D> baseGraph, uint edgeID, uint SplitAmount)
     {
         
     }
